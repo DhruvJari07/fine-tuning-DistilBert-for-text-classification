b70cfce9b6801573ce259a41daf7dd1b53a76bc3
DhruvJari07 <jariwaladhruv@yahoo.com>
2024-05-06 22:38:13 +0530

    readme

--- download_model.py
+++ download_model.py
@@ -4,8 +4,8 @@ from transformers import DistilBertModel, DistilBertTokenizer
 MODEL_NAME = "distilbert-base-uncased"
 # Download and save the pre-trained model
 model = DistilBertModel.from_pretrained(MODEL_NAME)
-model.save_pretrained("./Dhruv_code/premodels")
+model.save_pretrained("./premodels")
 # Download and save the tokenizer
 tokenizer = DistilBertTokenizer.from_pretrained(MODEL_NAME)
-tokenizer.save_pretrained("./Dhruv_code/premodels")
+tokenizer.save_pretrained("./premodels")
 print("Model and tokenizer downloaded successfully!")
